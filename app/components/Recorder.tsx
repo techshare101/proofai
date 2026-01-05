3c2c3d26ab298e42699bf12e100febc40102c849
Koll VossFam <valentin2v2000@gmail.com>
2026-01-05 10:40:20 -0600

    feat: add back to dashboard button on recorder page

--- app/components/Recorder.tsx
+++ app/components/Recorder.tsx
@@ -523,6 +523,19 @@ export default function Recorder() {
 
   return (
     <div className="flex flex-col items-center space-y-4 mt-8">
+      {/* Back to Dashboard Button */}
+      <div className="w-full max-w-lg flex justify-start mb-2">
+        <Link
+          href="/dashboard"
+          className="flex items-center gap-2 text-gray-600 hover:text-gray-900 transition"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+          </svg>
+          Back to Dashboard
+        </Link>
+      </div>
+
       <div className="relative w-full max-w-lg">
         <video
           ref={videoRef}
